fe7d25938ac3c5b8fe603e3c5ed5d9d49ef95ccb
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-04-11 14:07:33 -0300

    Upload project

---
+++ app/src/main/java/com/example/e_trash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.e_trash
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(3000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
