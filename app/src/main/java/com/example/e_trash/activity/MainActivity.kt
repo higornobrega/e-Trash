b945ec673f25976effe6ebf9dbfd653017622aca
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-05-09 19:00:22 -0300

    Add toolbar and menu

--- app/src/main/java/com/example/e_trash/activity/MainActivity.kt
+++ app/src/main/java/com/example/e_trash/activity/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.e_trash.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.e_trash.R
 import com.example.e_trash.helper.ConfiguracaoFirebase
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -14,23 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         auth = ConfiguracaoFirebase.getReferenciaAutenticacao()
 
-        bt_logout.setOnClickListener {
-            deslogarUsuario()
-            var intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        // Configurar a Toolbar
+        toolbarPrincipal.setTitle("e-Trash")
+        setSupportActionBar(toolbarPrincipal)
     }
 
     fun deslogarUsuario() {
         try {
             auth.signOut()
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
         catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menu_deslogar -> deslogarUsuario()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
