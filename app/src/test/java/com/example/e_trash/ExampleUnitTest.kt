65e8ce515615df28fee4c957bf1b5c470a416b5f
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-05-10 15:54:21 -0300

    NavigationBar add

--- app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
+++ app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.e_trash
 
+import com.example.e_trash.model.Usuario
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,18 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun createUser() {
+        val user = Usuario()
+        user.nome = "bruno"
+        user.email = "brunodesouza@gmail.com"
+        user.senha = "12345"
+
+        assertEquals(null, user.id)
+        assertEquals("bruno", user.nome)
+        assertEquals("brunodesouza@gmail.com", user.email)
+        assertEquals("Teste senha","12345", user.senha)
     }
 }
