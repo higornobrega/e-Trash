b0d622cdc8031e703c1fab3219c26af57019681c
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-05-20 16:17:19 -0300

    Consertando Bug

--- app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
@@ -2,6 +2,7 @@ package com.example.e_trash.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.e_trash.R;
 
 public class EditarPerfilActivity extends AppCompatActivity {
 
