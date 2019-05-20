7be1fdd82e1e6a3c57cecf9d2208d207a39605a3
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-05-20 16:09:17 -0300

    Editar perfilFragment

---
+++ app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
@@ -0,0 +1,13 @@
+package com.example.e_trash.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}
--- app/src/main/java/com/example/e_trash/fragment/PerfilFragment.java
+++ app/src/main/java/com/example/e_trash/fragment/PerfilFragment.java
@@ -1,13 +1,18 @@
 package com.example.e_trash.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import com.example.e_trash.R;
+import com.example.e_trash.activity.EditarPerfilActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +24,9 @@ public class PerfilFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Button bt_editar_perfil;
+    private ImageView avatar;
+    private EditText nomePerfil;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,7 +68,25 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        //Configuração do componentes
+        avatar = view.findViewById(R.id.avatar);
+        bt_editar_perfil = view.findViewById(R.id.bt_editar_perfil);
+        nomePerfil = view.findViewById(R.id.nomePerfil);
+
+        //Abre edição de perfil
+        avatar.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View v){
+                Intent i = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+
+                }
+
+    });
+
+        return view;
     }
 
 }
