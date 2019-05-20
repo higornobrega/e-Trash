0984269a9323ac383d628026f0b595e5c3ba03c4
Paulo Henrique <paulohenrique0953@gmail.com>
2019-05-20 19:40:09 -0300

    Correção de bug e botão voltar - Editar Perfil

--- app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
@@ -2,6 +2,7 @@ package com.example.e_trash.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 import com.example.e_trash.R;
 
 public class EditarPerfilActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class EditarPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+
+        //Botão voltar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
     }
+
+
 }
