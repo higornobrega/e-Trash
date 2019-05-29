83024a5bdcbbd7b1f8b0ef5034ff775dbe19eb35
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-05-29 18:15:34 -0300

    Warning fix

--- app/src/main/java/com/example/e_trash/activity/MainActivity.kt
+++ app/src/main/java/com/example/e_trash/activity/MainActivity.kt
@@ -12,17 +12,31 @@ import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.widget.Toast
 import com.example.e_trash.R
+import com.example.e_trash.helper.ConfiguracaoFirebase
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 	private lateinit var auth: FirebaseAuth
+	private lateinit var mMap: GoogleMap
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		val toolbar: Toolbar = findViewById(R.id.toolbar)
 		setSupportActionBar(toolbar)
+		auth = ConfiguracaoFirebase.getReferenciaAutenticacao()
+
+		val mapFragment = supportFragmentManager
+			.findFragmentById(R.id.map) as SupportMapFragment
+		mapFragment.getMapAsync(this)
 
 		val fab: FloatingActionButton = findViewById(R.id.fab)
 		fab.setOnClickListener { view ->
@@ -38,6 +52,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 		toggle.syncState()
 
 		navView.setNavigationItemSelectedListener(this)
+	}
+
+	/**
+	 * Manipulates the map once available.
+	 * This callback is triggered when the map is ready to be used.
+	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
+	 * we just add a marker near Sydney, Australia.
+	 * If Google Play services is not installed on the device, the user will be prompted to install
+	 * it inside the SupportMapFragment. This method will only be triggered once the user has
+	 * installed Google Play services and returned to the app.
+	 */
+	override fun onMapReady(googleMap: GoogleMap) {
+		mMap = googleMap
+
+		// Add a marker in Sydney and move the camera
+		val sydney = LatLng(-34.0, 151.0)
+		mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 	}
 
 	override fun onBackPressed() {
@@ -59,7 +91,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		when(item?.itemId) {
+		when(item.itemId) {
             R.id.action_logout -> deslogarUsuario()
         }
         return super.onOptionsItemSelected(item)
@@ -69,7 +101,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 		// Handle navigation view item clicks here.
 		when (item.itemId) {
 			R.id.nav_home -> {
-				// Handle the camera action
+				val intent = Intent(this, MapsActivity::class.java)
+				startActivity(intent)
 			}
 			R.id.nav_gallery -> {
 
@@ -95,12 +128,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 	fun deslogarUsuario() {
 		try {
 			auth.signOut()
+			Toast.makeText(applicationContext, "Deslogado com sucesso!", Toast.LENGTH_LONG).show()
 			var intent = Intent(this, LoginActivity::class.java)
 			startActivity(intent)
 			finish()
 		}
 		catch (e: Exception) {
 			e.printStackTrace()
+			Toast.makeText(applicationContext, "Erro ao deslogar!", Toast.LENGTH_LONG).show()
 		}
 	}
 }
