90f5299ea029f12160cfded93dc21aa6f10fbdab
Paulo Henrique <paulohenrique0953@gmail.com>
2019-06-06 18:46:28 -0300

    Alterando imagem de perfil e conexão com o firebase

--- app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/example/e_trash/activity/EditarPerfilActivity.java
@@ -1,14 +1,26 @@
 package com.example.e_trash.activity;
 
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.*;
 import com.example.e_trash.R;
+import com.example.e_trash.helper.ConfiguracaoFirebase;
 import com.example.e_trash.helper.UsuarioFirebase;
 import com.example.e_trash.model.Usuario;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class EditarPerfilActivity extends AppCompatActivity {
     private ImageView avatar;
@@ -16,13 +28,16 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private EditText editNomePerfil;
     private Button buttonSalvarAlteracoes;
     private Usuario usuarioLogado;
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
 
         usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
-
+        storageRef = ConfiguracaoFirebase.getFirebaseStorage();
         //Botão voltar
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
@@ -41,8 +56,65 @@ public class EditarPerfilActivity extends AppCompatActivity {
                 usuarioLogado.atualizar();
             }
         });
-        
+
+        //Alterar foto do perfil
+
+        textAlterarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try {
+                switch (requestCode) {
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+                if ( imagem != null){
+                    //Configura imagem na tela
+                    avatar.setImageBitmap(imagem);
+
+                    //Recuperar dados da imagem para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+
+                    //Salvar imagem no firebase
+                    StorageReference imagemRef = storageRef.child("imagens").child("perfil").child("<id-usuario>.jpeg");
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditarPerfilActivity.this,"Erro ao fazer upload da imagem.",Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(EditarPerfilActivity.this,"Sucesso ao fazer upload da imagem.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void inicializarComponentes(){
         avatar = findViewById(R.id.avatar);
         textAlterarFoto = findViewById(R.id.textAlterarFoto);
--- app/src/main/java/com/example/e_trash/helper/ConfiguracaoFirebase.java
+++ app/src/main/java/com/example/e_trash/helper/ConfiguracaoFirebase.java
@@ -3,11 +3,14 @@ package com.example.e_trash.helper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.*;
 
 public class ConfiguracaoFirebase {
 
     private static DatabaseReference referenciaFirebase;
     private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference storage;
+
 
     //retorna a referencia do database
     public static DatabaseReference getFirebase(){
@@ -24,4 +27,13 @@ public class ConfiguracaoFirebase {
         }
         return referenciaAutenticacao;
     }
+    public static StorageReference getFirebaseStorage(){
+        if( storage == null ){
+            storage = FirebaseStorage.getInstance().getReference();
+        }
+        return storage;
+    }
+
+
+
 }
