77e7bb739f0bc51cefe3d121920efa297ca0e678
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-06-13 00:46:01 -0300

    Teste de Editar lixo

--- app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
+++ app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
@@ -67,10 +67,58 @@ class ExampleUnitTest {
         assertEquals("bruno", user.nome)
         assertEquals("brunodesouza@gmail.com", user.email)
         assertEquals("Teste senha","12345", user.senha)
+
         user.nome = "bbrruunnoo"
+        user.email = "bruno@gmail.com"
+        user.senha = "12345abc"
         assertEquals(null, user.id)
         assertEquals("bbrruunnoo", user.nome)
-        assertEquals("brunodesouza@gmail.com", user.email)
-        assertEquals("Teste senha","12345", user.senha)
+        assertEquals("bruno@gmail.com", user.email)
+        assertEquals("Teste senha","12345abc", user.senha)
     }
+    @Test
+    fun AtualizarLixo(){
+        var user = Usuario()
+        user.nome = "bruno"
+        user.email = "brunodesouza@gmail.com"
+        user.senha = "12345"
+
+        val lixo = Lixo();
+        lixo.nome = "Lixo1"
+        lixo.longitude = -52.4481016
+        lixo.latitude = -23.0720325
+        lixo.informacoes = "Informações lixo1"
+        lixo.idUsuario = user.id
+        lixo.endereco = "Rua Lixo1"
+        lixo.caminhoFoto = "Caminho foto lixo1"
+
+        assertEquals(null, lixo.id)
+        assertEquals("Lixo1", lixo.nome)
+        //assertEquals(-52.4481016, lixo.longitude)
+        //assertEquals(-23.0720325, lixo.latitude)
+        assertEquals("Informações lixo1", lixo.informacoes)
+        assertEquals(user.id, lixo.idUsuario)
+        assertEquals("Rua Lixo1", lixo.endereco)
+        assertEquals("Caminho foto lixo1", lixo.caminhoFoto)
+
+        lixo.nome = "Lixo1.1"
+        lixo.longitude = -52.4481016
+        lixo.latitude = -23.0720325
+        lixo.informacoes = "Informações lixo1.1"
+        lixo.idUsuario = user.id
+        lixo.endereco = "Rua Lixo1.1"
+        lixo.caminhoFoto = "Caminho foto lixo1.1"
+
+        assertEquals(null, lixo.id)
+        assertEquals("Lixo1.1", lixo.nome)
+        //assertEquals(-52.4481016, lixo.longitude)
+        //assertEquals(-23.0720325, lixo.latitude)
+        assertEquals("Informações lixo1.1", lixo.informacoes)
+        assertEquals(user.id, lixo.idUsuario)
+        assertEquals("Rua Lixo1.1", lixo.endereco)
+        assertEquals("Caminho foto lixo1.1", lixo.caminhoFoto)
+
+    }
+
+
 }
