e987e17bb1469f78f8f6870d9274a067cfb07284
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-06-12 22:28:00 -0300

    Adicionando marcador ao mapa quando é adicionado ponto de coleta

--- app/src/main/java/com/example/e_trash/activity/AdicionarPontoDeColeta.java
+++ app/src/main/java/com/example/e_trash/activity/AdicionarPontoDeColeta.java
@@ -1,5 +1,12 @@
 package com.example.e_trash.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,11 +18,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class AdicionarPontoDeColeta extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     private EditText nomePontoColeta;
     private Button botaoSalvar;
     private Button botaoCancelar;
@@ -41,17 +51,53 @@ public class AdicionarPontoDeColeta extends FragmentActivity implements OnMapRea
     public void inicializarComponentes() {
         nomePontoColeta = findViewById(R.id.et_nome_ponto_coleta);
         botaoSalvar = findViewById(R.id.bt_salvar_ponto_coleta);
-        botaoCancelar =findViewById(R.id.bt_cancelar_ponto_coleta);
+        botaoCancelar = findViewById(R.id.bt_cancelar_ponto_coleta);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+        recuperarLocalizacaoUsuario();
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.lixeira)));
+            }
+        });
+    }
 
+    private void recuperarLocalizacaoUsuario() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng meuLocal = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(meuLocal).title("Você está aqui").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(meuLocal, 15));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 120000, 5, locationListener);
+        }
     }
 }
