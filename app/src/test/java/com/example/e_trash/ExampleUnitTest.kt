121265c6e34639636878614b4b824b9044006b6a
Higor Nóbrega <higorst.nobrega@gmail.com>
2019-06-13 00:28:54 -0300

    Teste de criar lixo e Atualizar usuario

--- app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
+++ app/src/test/java/com/example/e_trash/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package com.example.e_trash
 
 import com.example.e_trash.model.Usuario
+import com.example.e_trash.model.Lixo
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -17,13 +18,58 @@ class ExampleUnitTest {
 
     @Test
     fun createUser() {
-        val user = Usuario()
+        var user = Usuario()
         user.nome = "bruno"
         user.email = "brunodesouza@gmail.com"
         user.senha = "12345"
 
         assertEquals(null, user.id)
         assertEquals("bruno", user.nome)
+        assertEquals("brunodesouza@gmail.com", user.email)
+        assertEquals("Teste senha","12345", user.senha)
+
+        //user = null
+
+    }
+    @Test
+    fun createLixo(){
+        var user = Usuario()
+        user.nome = "bruno"
+        user.email = "brunodesouza@gmail.com"
+        user.senha = "12345"
+
+        val lixo = Lixo();
+        lixo.nome = "Lixo1"
+        lixo.longitude = -52.4481016
+        lixo.latitude = -23.0720325
+        lixo.informacoes = "Informações lixo1"
+        lixo.idUsuario = user.id
+        lixo.endereco = "Rua Lixo1"
+        lixo.caminhoFoto = "Caminho foto lixo1"
+
+        assertEquals(null, lixo.id)
+        assertEquals("Lixo1", lixo.nome)
+        //assertEquals(-52.4481016, lixo.longitude)
+        //assertEquals(-23.0720325, lixo.latitude)
+        assertEquals("Informações lixo1", lixo.informacoes)
+        assertEquals(user.id, lixo.idUsuario)
+        assertEquals("Rua Lixo1", lixo.endereco)
+        assertEquals("Caminho foto lixo1", lixo.caminhoFoto)
+
+    }
+    @Test
+    fun AtualizarUsuario(){
+        var user = Usuario()
+        user.nome = "bruno"
+        user.email = "brunodesouza@gmail.com"
+        user.senha = "12345"
+        assertEquals(null, user.id)
+        assertEquals("bruno", user.nome)
+        assertEquals("brunodesouza@gmail.com", user.email)
+        assertEquals("Teste senha","12345", user.senha)
+        user.nome = "bbrruunnoo"
+        assertEquals(null, user.id)
+        assertEquals("bbrruunnoo", user.nome)
         assertEquals("brunodesouza@gmail.com", user.email)
         assertEquals("Teste senha","12345", user.senha)
     }
